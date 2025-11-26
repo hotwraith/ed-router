c5abaa0afbd665ec8e3a81cfc7e735362f840147
hotwraith <hotwraith4803@gmail.com>
2025-11-26 18:25:34 +0100

    [EDIT] build.py Reduced scope to this project only, added "debug" mode that builds the .exe keeping the console for debugging

--- build.py
+++ build.py
@@ -7,7 +7,9 @@ from dotenv import load_dotenv
 
 class Builder():
 
-    def __init__(self) -> None:
+    def __init__(self, debug=False) -> None:
+        global isDebug
+        self.isDebug = debug
         self.build()
 
     def build(self):
@@ -16,6 +18,12 @@ class Builder():
         self.create_directory(version=version)
         self.merge_internal(compiled, version)
         self.create_specific(version)
+
+
+    def remove_old_builds(self, pyFile:str) -> None:
+        if(pyFile.removesuffix('.py') in os.listdir('dist')):
+            shutil.rmtree(os.path.join('dist', pyFile.removesuffix('.py')))
+
 
     def create_directory(self, version:str) -> None:
         if(version in os.listdir('dist')):
@@ -48,8 +56,11 @@ class Builder():
             if '.py' in el and 'build' not in el:
                 toCompile.append(el)
         for el in toCompile:
-            subprocess.run(['pyinstaller', '-w', el])
-            #subprocess.run(['pyinstaller', el])
+            self.remove_old_builds(el)
+            if(self.isDebug):
+                subprocess.run(['pyinstaller', el])
+            else:
+                subprocess.run(['pyinstaller', '-w', el, '-i', 'edrouter.ico'])
         return toCompile
 
 
@@ -59,24 +70,15 @@ class Builder():
     
     def create_specific(self, version) -> None:
         working_directory = os.path.join('dist', version)
-        running = True
-        while running:
-            newFile = input("File name (with extension): ")
-            if(newFile.upper() != 'EXIT'):
-                with open(os.path.join(working_directory, newFile), 'w') as f:
-                    run2 = True
-                    lines = []
-                    while run2:
-                        line = input("Line content: ")
-                        if(line.upper() != 'EXIT'):
-                            lines.append(line)
-                        else:
-                            run2 = False
-                    for el in lines:
-                        f.write(el+'\n')
-                    f.close()
-            else:
-                running = False
+        with open(os.path.join(working_directory, '.env'), 'w') as f:
+            f.write("OUTPUT_PATH=\'%USERPROFILE%\\Downloads\\\'")
+            f.close()
+        with open(os.path.join(working_directory, 'systems.txt'), 'w') as f:
+            f.write('Start system\n')
+            f.write('Other system\n')
+            f.write('Other system\n')
+            f.write('Other system')
+            f.close()
 
 if __name__ == '__main__':
     Builder()
